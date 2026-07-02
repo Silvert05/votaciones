16448ed836ef866cc20507c79268f0a0f45d6c80
David <cochadavid2005@gmail.com>
2026-07-02 09:58:22 -0500

    update_logos_colores

--- apps/web/src/app/features/website/pages/actividades/actividades.ts
+++ apps/web/src/app/features/website/pages/actividades/actividades.ts
@@ -12,20 +12,20 @@ export default class ActividadesComponent {
     {
       fecha: '26/09/2022',
       actividad: [
-        { title: 'Reunión de trabajo con los órganos de la FIERPI', responsable: 'PRESIDENTE T.E ', lugar: 'CASA FIERPI' },
-        { title: 'Enviar oficios solicitando la base de datos a las iglesias conformar el padrón electoral', responsable: 'SECRETARIO', lugar: 'CASA FIERPI' }
+        { title: 'Reunión de trabajo con los órganos del instituto yavirac', responsable: 'PRESIDENTE T.E ', lugar: 'INSTITUTO YAVIRAC' },
+        { title: 'Enviar oficios solicitando la base de datos del instituto para conformar el padrón electoral', responsable: 'SECRETARIO', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
-    {
+    /*{
       fecha: '28/09/2022',
       actividad: [
-        { title: 'Rueda de prensa con todos los medios alternativos de las iglesias de la FIERPI', responsable: 'TRIBUNAL ELECTORAL', lugar: 'CASA FIERPI', hora: '17:30' }
+        { title: 'Rueda de prensa con todos los medios alternativos de las iglesias de la FIERP', responsable: 'TRIBUNAL ELECTORAL', lugar: 'CASA FIERPI', hora: '17:30' }
       ]
-    },
+    },*/
     {
       fecha: '03/10/2022',
       actividad: [
-        { title: 'Recepción de la base de datos', responsable: 'SECRETARIA Y TRIBUNAL ELECTORAL', lugar: 'CASA FIERPI', hora: '19:00' }
+        { title: 'Recepción de la base de datos', responsable: 'SECRETARIA Y TRIBUNAL ELECTORAL', lugar: 'INSTITUTO YAVIRAC', hora: '19:00' }
       ]
     },
     {
@@ -37,25 +37,25 @@ export default class ActividadesComponent {
     {
       fecha: '12/10/2022',
       actividad: [
-        { title: 'Asamblea General REFORMA DEL REGLAMENTO', responsable: 'PRESIDENTE TRIBUNAL', lugar: 'CASA FIERPI', hora: '17:00' }
+        { title: 'Asamblea General REFORMA DEL REGLAMENTO', responsable: 'PRESIDENTE TRIBUNAL', lugar: 'INSTITUTO YAVIRAC', hora: '17:00' }
       ]
     },
     {
       fecha: '19/10/2022',
       actividad: [
-        { title: 'Inscripciones de candidatos', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Inscripciones de candidatos', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
       fecha: '24/10/2022',
       actividad: [
-        { title: 'Calificación de los candidatos', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Calificación de los candidatos', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
       fecha: '26/10/2022',
       actividad: [
-        { title: 'Asamblea General Presentar formalmente a  las listas y la socialización', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Asamblea General Presentar formalmente a  las listas y la socialización', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
@@ -73,25 +73,25 @@ export default class ActividadesComponent {
     {
       fecha: '14/11/2022 A 25/11/2022',
       actividad: [
-        { title: 'Conformación de las juntas receptoras de votos', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Conformación de las juntas receptoras de votos', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
       fecha: '27/11/2022',
       actividad: [
-        { title: 'Elecciones y proclamación de las nuevas autoridades DECLARADO DÍA CÍVICO DE LA FIERI', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Elecciones y proclamación de las nuevas autoridades DECLARADO DÍA CÍVICO DL YAVIRAC', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
       fecha: '28/11/2022',
       actividad: [
-        { title: 'Impugnaciones', responsable: 'TRIBUNAL', lugar: 'CASA FIERPI' }
+        { title: 'Impugnaciones', responsable: 'TRIBUNAL', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
       fecha: '30/11/2022',
       actividad: [
-        { title: 'Planificación del posicionamiento de las nuevas autoridades', responsable: 'TRIBUNAL Y LAS NUEVAS AUTORIDADES', lugar: 'CASA FIERPI' }
+        { title: 'Planificación del posicionamiento de las nuevas autoridades', responsable: 'TRIBUNAL Y LAS NUEVAS AUTORIDADES', lugar: 'INSTITUTO YAVIRAC' }
       ]
     },
     {
